855fe221dd5cce117dfe2eae5ddcc7c8f3e85f12
Abhijeet Singh <singhabhijeet732@gmail.com>
2024-06-21 17:05:49 +0530

    Added vercel.json

--- index.js
+++ index.js
@@ -84,7 +84,9 @@ app.get("/questions/:topic", (req, res) => {
   }
 });
 
-const PORT = 5001;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+module.exports = app;
